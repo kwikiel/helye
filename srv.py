fe97fbcbbf1ce70a3345a8f669728467bdeb8f9c
Michał 'czesiek' Czyżewski <me@czesiek.net>
2013-04-16 20:11:57 +0200

    flask -> bottle, less bloat

--- srv.py
+++ srv.py
@@ -1,54 +1,26 @@
-from flask import (Flask, request, redirect, render_template)
-import flask
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from bottle import route, get, post, run, static_file, template, request
 import json
 import random
 
 from output import array
 
-app = Flask(__name__)
-
-def jsonify(data):
-    return flask.Markup(json.dumps(data))
-
-app.jinja_env.filters['jsonify'] = jsonify
-
-@app.route('/<int:first_id>/<int:second_id>/<int:third_id>')
-@app.route('/')
+@get('/')
+@get('/<first_id:int>/<second_id:int>/<third_id:int>')
 def home(first_id=None, second_id=None, third_id=None):
-    return render_template('home.html')
+    return template("home")
 
-@app.route('/api/words')
+@get('/api/words')
 def api_words():
-    # NOTE: below is old code from route('/') up for a review
-    #
-    #'''
-    #    print len(array)
-    #while first_id == None:
-    #    rand_int = randint(0, len(array)-1)
-    #    value = array[rand_int]
-    #    if len(value) > 0:
-    #        first_id = rand_int
-    #while second_id == None:
-    #    rand_int = randint(0, len(array)-1)
-    #    value = array[rand_int]
-    #    if len(value) > 0:
-    #        second_id = rand_int
-    #'''
-    #
-    #def get_words(words):
-    #    ret = []
-    #    for word_id in words:
-    #        word = array[word_id] if word_id != None and word_id < len(array) else random.choice(array)
-    #        ret.append(word.decode('utf-8'))
-    #    return ret
-
     word1 = random.choice(array);
     word2 = random.choice(array);
     word3 = random.choice(array);
 
     return json.dumps([word1, word2, word3])
 
-@app.route('/api/save', methods=['POST'])
+@post('/api/save')
 def api_save():
     # TODO: This is a stub
 
@@ -57,7 +29,7 @@ def api_save():
     err = None
 
     try:
-        image_url = request.form['image_url']
+        image_url = request.forms.image_url
     except KeyError:
         err = "No image_url provided"
 
@@ -68,9 +40,12 @@ def api_save():
     print image_url
     return "Saved!"
 
-@app.route('/manifest.webapp')
+@get('/manifest.webapp')
 def manifest():
-    return open('manifest.webapp').read()
+    return static_file('manifest.webapp', root='.')
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=6969, debug=True)
+@get('/static/<path:path>')
+def serve_static(path):
+    return static_file(path, root='./static/')
+
+run(host='localhost', port=6969, reloader=True)
